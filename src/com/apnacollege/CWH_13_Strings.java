2af4d57790a886e7aefc4bd28c31e04fd5a52fa7
Jatin666 <verma.jatin6661@gmail.com>
2022-07-02 13:01:41 +0200

    new

---
+++ src/com/apnacollege/CWH_13_Strings.java
@@ -0,0 +1,14 @@
+package com.apnacollege;
+
+public class CWH_13_Strings {
+    public static void main(String[] args) {
+        //String name = new String("Jatin");
+        String name ="Jatin";
+        System.out.println(name);
+
+        int a = 6;
+        float b = 5.4345f;
+        System.out.printf("The value of a is %d and the value of b is %8.2f", a,b); //string method
+    }
+
+}
